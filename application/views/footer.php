c4747d412ac2eebb48b5e4578fe7e2c99e3184cc
Miguel Angel Castillo Cornejo <mikedosce1992@gmail.com>
2015-12-13 17:28:41 -0600

    Archivos inecesarios eliminados

--- application/views/footer.php
+++
@@ -1,5 +0,0 @@
-		<footer>
-			<p>Copyright © 2014-2015 Universidad Nacional de Ingeniería - Recinto Universitario Augusto César Sandino. Todos los derechos reservados.</p>
-		</footer>
-	</body>
-</html>
