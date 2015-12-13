38e046dae59f06a5df2db40cc5d60aacb4fc3b45
Miguel Angel Castillo Cornejo <mikedosce1992@gmail.com>
2015-12-12 21:55:27 -0600

    Imagen compuesta en importar egresados

--- application/views/panel/importar/importar_egresados.php
+++ application/views/panel/importar/importar_egresados.php
@@ -22,7 +22,7 @@
 					<h4>Antes de subir un archivo al servidor tenga en cuenta las siguientes consideraciones:</h4>
 					<ul>
 						<li><p>El archivo debe de tener el formato adecuado tal y como se muestra a continuación:</p>
-						<img class="img-responsive" src="<?=base_url('dist/img/formato_excel.jpg')?>" alt="La imagen no puede ser visualizada."></li>
+						<img style="width:100%;" src="<?=base_url('dist/img/formato_excel.jpg')?>" alt="La imagen no puede ser visualizada."></li>
 						<li>La primera fila del archivo debe de contener las cabeceras mostradas en la imagen, estan no son leidas por el sistema pero son necesarias para identificar el formato de este.</li>
 						<li>Las fechas proporcionadas deben de ir separadas por guiones y no por espacios o plecas a como se muestra en este ejemplo: 12-12-2005</li>
 						<li>Asegurese de evitar los maximos errores posibles para que el sistema no presente problemas.</li>
